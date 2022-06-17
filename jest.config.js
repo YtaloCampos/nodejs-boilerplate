ad5ef30aa1bc469d1871f3db52cfb885c49b1af4
Ytalo Campos <ytalo.campos@mutual.club>
2022-06-17 13:08:16 -0300

    keep tests folder

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
-
+/* eslint-disable */
 module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1'
+    '@/(.+)': '<rootDir>/src/$1',
   },
-  roots: [
-    '<rootDir>/src',
-    '<rootDir>/tests'
-  ],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   transform: {
-    '\\.ts$': 'ts-jest'
-  }
-}
+    '\\.ts$': 'ts-jest',
+  },
+};
